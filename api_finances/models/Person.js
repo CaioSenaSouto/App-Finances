a8507e2c4063d60f91887ad5959a6fe7add88c14
MartaTonet <martadaconceicaotonet@gmail.com>
2021-12-12 14:22:47 -0300

    Criação dos crudes e inserção de dados no banco

--- api_finances/models/Person.js
+++ api_finances/models/Person.js
@@ -1,9 +1,22 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const Person = mongoose.model('Person', {
-  name: String,
-  balance: Number,
-  approved: Boolean
-})
+// const Person = mongoose.model('Person', {
+//   name: String,
+//   balance: Number,
+//   approved: Boolean
+// })
 
-module.exports = Person
+// module.exports = Person
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const pessoa = new schema({
+  nome: {type: String, required:true},
+  email: {type: String, required:true},
+  aprovado: {type:Boolean, default:false},
+  created_at: {type:Date, default: Date.now}
+});
+
+//Criando a colletion com nome clientes
+module.exports = mongoose.model("lancamentos", pessoa)
