452d60eaecb5694d501a9305b964d9dc772117aa
Taila Lima <tailalima.ds@gmail.com>
2021-12-12 17:01:01 -0300

    Validação de campos vazios

--- api_finances/api.js
+++ api_finances/api.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const mongoose = require('mongoose');
 const porta = 3000
 
 const Person = require('./models/Person')
@@ -9,25 +8,25 @@ const Person = require('./models/Person')
 // forma de ler JSON / middlewares
 
 app.use(
-  express.urlencoded({
-    extended: true
-  })
-)
-//get para testar
-app.get('/lancamentos', function(req,res){
-    res.send('Buscar lamençamentos')
+        express.urlencoded({
+            extended: true
+        })
+    )
+    //get para testar
+app.get('/lancamentos', function(req, res) {
+    res.send('Buscar lançamentos')
 })
 
 
 //conectando o index ao banco de dados usando o código a baixo
-mongoose.connect('mongodb://localhost/financa').then(function(){
+mongoose.connect('mongodb://localhost/financa').then(function() {
     console.log(`Conectado ao banco de dados`)
 
     app.listen(porta, function() {
         console.log(`rodando app na url http://localhost:${porta}/lancamentos`)
     })
 
-}).catch(function(erro){
+}).catch(function(erro) {
     console.log(`Falha ao conectar-se:${erro}`)
 })
 
@@ -35,30 +34,35 @@ mongoose.connect('mongodb://localhost/financa').then(function(){
 app.use(express.json())
 
 //rotas da API
-app.post('/person', async (req, res) => {
-  //req.body
-  const { name, balance, approved } = req.body
+app.post('/person', async(req, res) => {
+    //req.body
+    const { name, balance, approved } = req.body
 
-  const person = {
-    name,
-    balance,
-    approved
-  }
-  try {
-    //criando dados
-    await Person.create(person)
+    const person = {
+        name,
+        balance,
+        approved
+    }
+    try {
+        if (name && balance && approved) {
+            //criando dados
+            await Person.create(person)
 
-    res.status(201).json({ msg: 'Pessoa inserida no sistema com sucesso!' })
-  } catch (error) {
-    res.status(500).json({ error: error })
-  }
+            res.status(201).json({ msg: 'Pessoa inserida no sistema com sucesso!' })
+        } else {
+            res.status(500).json({ error: "É necessário preencher todos os campos." })
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 })
 
 // rota inicial / endpoint
 app.get('/', (req, res) => {
-  // mostrar req
+    // mostrar req
 
-  res.json({ msg: 'Hello Express!' })
+    res.json({ msg: 'Hello Express!' })
 })
 
 // @qEmd9Ntf!cKt.A
@@ -70,26 +74,26 @@ const DB_USER = 'Time05'
 const DB_PASSWORD = '%40qEmd9Ntf!cKt.A'
 
 mongoose
-  .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.klywl.mongodb.net/bancodaapi?retryWrites=true&w=majority`
-  )
-  .then(() => {
-    console.log('Conectamos ao MongoDB!')
-    app.listen(3000)
-  })
-  .catch(err => console.log(err))
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.klywl.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log('Conectamos ao MongoDB!')
+        app.listen(3000)
+    })
+    .catch(err => console.log(err))
 
 app.get('/lancamentos', (req, res) => {
-  res.send({ message: 'Hello Express!' })
+    res.send({ message: 'Hello Express!' })
 })
 
-mongoose.connect('mongodb://localhost/financa').then(function(){
+mongoose.connect('mongodb://localhost/financa').then(function() {
     console.log(`Conectado ao banco de dados`)
 
     app.listen(porta, function() {
         console.log(`rodando app na url http://localhost:${porta}/lancamentos`)
     })
 
-}).catch(function(erro){
+}).catch(function(erro) {
     console.log(`Falha ao conectar-se:${erro}`)
 })
