d475c2ecc8512b7d89f543141b69581918403c20
Gessica Araujo <araujo.gessica@gmail.com>
2021-12-11 22:18:10 -0300

    Corrigindo funcao de validacao dos campos de entrada do usuario

--- api_finances/js/lancamentos.js
+++ api_finances/js/lancamentos.js
@@ -2,12 +2,11 @@ const campos = document.querySelectorAll("[required]");
 
 function validacao(e) {
     const campo = e.target
-
     function verificandoErro() {
         let erroEncontrado = false;
 
         for(let erro in campo.validity) {
-            if (erro != "customError" && campo.validity[erro]) {
+            if (erro != "customError" && campo.validity[erro] && erro!= "valid") {
                 erroEncontrado = erro;
             }
         }
@@ -19,6 +18,7 @@ function validacao(e) {
     if (erro) {
         campo.setCustomValidity("Hey, não esquece de mim :(")
         campo.style.border = "3px solid rgb(255, 56, 56)"
+        campo.focus()
     } else {
         campo.setCustomValidity("")
         campo.style.border = "3px solid rgb(12, 165, 165)"
@@ -26,7 +26,11 @@ function validacao(e) {
 }
 
 for (let campo of campos) {
-    campo.addEventListener("invalid", validacao)
+    campo.addEventListener("invalid", event=> {
+        event.preventDefault
+        validacao(event)
+    })
+    campo.addEventListener("blur", validacao)
 }
 
 document.querySelector("form").addEventListener("submit", e => { 
@@ -34,14 +38,16 @@ document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault()
 })
 
+
+//function casas decimais
 function formatarEntrada() {
     var elemento = document.getElementById('valor');
     var valor = elemento.value;
     // var valorFormatado = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     
-
     valor = valor + '';
-    valor = parseInt(valor.replace(/[\D]+/g, ''));
+    valor = valor.replace(/[\D]+/g, '');
+    console.log(valor)
     valor = valor + '';
     valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
@@ -51,6 +57,5 @@ function formatarEntrada() {
 
     elemento.value = valor;
 
-    if(valor == 'NaN') elemento.value = '';
-    
+    if(valor == 'NaN') elemento.value = ''
 }
