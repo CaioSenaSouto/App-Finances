0169619ac0b4865e4090a586b9325a2dd9023843
Gessica Araujo <araujo.gessica@gmail.com>
2021-12-13 16:11:19 -0300

    Corrigindo problema de href do botao de novos lancamentos.

--- api_finances/js/lancamentos.js
+++ api_finances/js/lancamentos.js
@@ -1,3 +1,4 @@
+//function validação de campos
 const campos = document.querySelectorAll("[required]");
 
 function validacao(e) {
@@ -33,29 +34,58 @@ for (let campo of campos) {
     campo.addEventListener("blur", validacao)
 }
 
-document.querySelector("form").addEventListener("submit", e => { 
-    console.log("Enviar formulário");
-    e.preventDefault()
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault() 
+    location.href = './tabela.html';
 })
 
 
-//function casas decimais
-function formatarEntrada() {
-    var elemento = document.getElementById('valor');
-    var valor = elemento.value;
-    // var valorFormatado = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+// function casas decimais
+// function formatarEntrada() {
+//     var elemento = document.getElementById('valor');
+//     var valor = elemento.value;
+//     console.log(valor)
+//     // var valorFormatado = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     
-    valor = valor + '';
-    valor = valor.replace(/[\D]+/g, '');
-    console.log(valor)
-    valor = valor + '';
-    valor = valor.replace(/([0-9]{2})$/g, ",$1");
+//     valor = valor + '';
+//     valor = valor.replace(/[\D]+/g, '');
+//     valor = valor + '';
+//     valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+//     if (valor.length > 6) {
+//         valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+//     }
+
+//     elemento.value = valor;
+
+//     if(valor == 'NaN') elemento.value = ''
+// }
+
+function radioBox() {
+    const radioSaida = document.getElementById("radio-saida")
+
+    if(radioSaida.checked) {
+        console.log("saindo. acrescentar um negativo à esquerda")
     }
-
-    elemento.value = valor;
-
-    if(valor == 'NaN') elemento.value = ''
 }
+
+
+function formatarEntrada() {
+    let valor = document.getElementById("valor").value
+    // let valorEntrada = valor.value
+    // console.log(valorEntrada)
+    var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+      });
+    //   console.log(formatter.format(2500)); /* $2,500.00 */
+    valor = formatter.format(valor)
+    valor = valor
+    console.log(valor)
+    // (valorEntrada).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
