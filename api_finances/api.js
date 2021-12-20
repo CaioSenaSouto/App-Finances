22fd96c9262cdf71c76a6640131b53c9176e0ac6
Gessica Araujo <araujo.gessica@gmail.com>
2021-12-20 18:36:57 -0300

    Removendo dependencia CORS

--- api_finances/api.js
+++ api_finances/api.js
@@ -1,11 +1,9 @@
-// const cors = require('cors')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
 const porta = process.env.PORT || 3000
 
-// app.use(cors())
 app.use(express.json())
 
 const Person = require('./models/people')
