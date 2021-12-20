9b2f1f504c7332b099d15d15c22a0b8f0c5c508b
Gessica Araujo <araujo.gessica@gmail.com>
2021-12-20 09:39:38 -0300

    Adicionando function para receber valor de saldo na tela de novos lancamentos

--- frontATUALIZADA/js/global.js
+++ frontATUALIZADA/js/global.js
@@ -1,59 +1,10 @@
-// const usuarioLogado = (id) => {
-//     fetch(`http://localhost:3000/people/${id}`)
-//     .then(x => x.text())
-//     .then(JSON.parse)
-//     .then(dado => {
-//         document.getElementById("usuario").innerText = dado.pessoa.nome
-//     })
-// }
-
-// const idCaio = "61b9fd3c0870d00cc83a7dfa"
-// const idMarta = "61b9fd0e0870d00cc83a7df6"
-// const idPam = "61b9fd500870d00cc83a7dfc"
-// const idGe = "61b9fb2a0870d00cc83a7def"
-// const idGerman = "61b9fd5f0870d00cc83a7dfe"
-// const idTai = "61b9fd250870d00cc83a7df8"
-
-// // usuarioLogado(idGe)
-// usuarioLogado(idCaio)
-
-// alterar para receber array de lançamentos do usuário
-// const telaInicio = (email) => {
-//     fetch(`http://localhost:3000/people/email/transaction/${email}`)
-//         .then(x => x.text())
-//         .then(JSON.parse)
-//         .then(dado => {
-//             let usuarioLancamentos = document.querySelector(".sem-lancamentos")
-//             let usuarioSemLancamentos = document.querySelector(".com-lancamentos")
-
-//             if (dado.transacao.length <= 0) {
-//                 usuarioLancamentos.style.display = "none"
-//                 usuarioSemLancamentos.style.display = "inline"
-//             } else {
-//                 usuarioSemLancamentos.style.display = "none"
-//                 usuarioLancamentos.style.display = "inline"
-//             }
-//         })
-
-// }
-
-// const emailMarta = "martadaconceicaotonet@gmail.com"
-// const emailCaio = "caio@kkkrying.com"
-// const emailGe = "gessica@meuemail.com"
-// const emailTai = "tailalima.ds@gmail.com"
-// const emailGerman = "german@neon.com"
-// const emailPam = "pamela@dosteclados.com"
-
-// telaInicio(emailGe)
-// telaInicio(emailCaio)
-
 let allTransactions = []
 
 let match = [];
 
 const databaseSearch = {
   usuario: {
-    email: 'luis@luis.com'
+    email: 'tailalima.ds@gmail.com'
   },
     async porUsuario() {
         let url = 'http://localhost:3000/people/:email/transaction'
@@ -108,7 +59,14 @@ const Transactions = {
   },
 
   total() {
-      return this.incomes() + this.expenses();
+    let saldo = this.incomes() + this.expenses()
+
+    if (saldo > 0) {
+      document.getElementById("dinheiro").style.color = "green"
+    } else if (saldo < 0) {
+      document.getElementById("dinheiro").style.color = "red"
+    }
+    return saldo.toFixed(2)
   },
 }
 
