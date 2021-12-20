c9f405b4ed938449a0b76d9c227683b971ce64e6
Caio Sena <caio.sena22@gmail.com>
2021-12-20 17:32:29 -0300

    Retirando da url localhost3000

--- frontATUALIZADA/js/global.js
+++ frontATUALIZADA/js/global.js
@@ -1,94 +1,92 @@
 let allTransactions = []
 
-let match = [];
+let match = []
 
 const databaseSearch = {
   usuario: {
     email: 'tailalima.ds@gmail.com'
   },
-    async porUsuario() {
-        let url = 'http://localhost:3000/people/:email/transaction'
-        transcoes = await fetch(url).then(x => x.text())
-            .then(JSON.parse)
-            .then(data => {
-                todasTransacoes = data.transacao
-                todasTransacoes.forEach(dado => {
-                    if (dado.email === this.usuario.email) {
-                        match.push(dado)
-                    }
-                })
-                carregaTransacoes();
-            })
-
-    }
+  async porUsuario() {
+    let url = '/people/:email/transaction'
+    transcoes = await fetch(url)
+      .then(x => x.text())
+      .then(JSON.parse)
+      .then(data => {
+        todasTransacoes = data.transacao
+        todasTransacoes.forEach(dado => {
+          if (dado.email === this.usuario.email) {
+            match.push(dado)
+          }
+        })
+        carregaTransacoes()
+      })
+  }
 }
 
 function carregaTransacoes() {
   match.forEach(transacao => {
-      allTransactions.push({
-          titulo: transacao.descricao,
-          custo: transacao.valor,
-          data: transacao.data,
-          id: transacao._id
-      })
+    allTransactions.push({
+      titulo: transacao.descricao,
+      custo: transacao.valor,
+      data: transacao.data,
+      id: transacao._id
+    })
   })
 }
 const Transactions = {
   all: allTransactions,
   incomes() {
-      let income = 0;
+    let income = 0
 
-      this.all.forEach(transaction => {
-          if (transaction.custo > 0) {
-              income += transaction.custo;
-          }
-      })
-      return income;
+    this.all.forEach(transaction => {
+      if (transaction.custo > 0) {
+        income += transaction.custo
+      }
+    })
+    return income
   },
 
   expenses() {
-    let expense = 0;
-    
-    this.all.forEach(transaction => {
-        if (transaction.custo < 0) {
-            expense += transaction.custo;
-        }
-    })
-      return expense;
+    let expense = 0
 
+    this.all.forEach(transaction => {
+      if (transaction.custo < 0) {
+        expense += transaction.custo
+      }
+    })
+    return expense
   },
 
   total() {
     let saldo = this.incomes() + this.expenses()
 
     if (saldo > 0) {
-      document.getElementById("dinheiro").style.color = "green"
+      document.getElementById('dinheiro').style.color = 'green'
     } else if (saldo < 0) {
-      document.getElementById("dinheiro").style.color = "red"
+      document.getElementById('dinheiro').style.color = 'red'
     }
     return saldo.toFixed(2)
-  },
+  }
 }
 
 const balanceComponent = {
-  
   formatCurrency(value) {
-    const signal = Number(value) < 0 ? "-" : ""
+    const signal = Number(value) < 0 ? '-' : ''
 
-    value = String(value).replace(/\D/g, "")
+    value = String(value).replace(/\D/g, '')
     value = Number(value) / 100
-    value = value.toLocaleString("pt-BR", {
-        style: "currency",
-        currency: "BRL"
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
     })
     return signal + value
   },
 
   updateBalance() {
-    document.getElementById('dinheiro')
-        .innerHTML = this.formatCurrency(Transactions.total())
-    
-  },
+    document.getElementById('dinheiro').innerHTML = this.formatCurrency(
+      Transactions.total()
+    )
+  }
 }
 
 window.onload = async () => {
--- frontATUALIZADA/js/lancamentos.js
+++ frontATUALIZADA/js/lancamentos.js
@@ -1,73 +1,77 @@
 //function validação de campos
-const campos = document.querySelectorAll("[required]");
+const campos = document.querySelectorAll('[required]')
 
 function validacao(e) {
-    const campo = e.target
+  const campo = e.target
 
-    function verificandoErro() {
-        let erroEncontrado = false;
+  function verificandoErro() {
+    let erroEncontrado = false
 
-        for (let erro in campo.validity) {
-            if (erro != "customError" && campo.validity[erro] && erro != "valid") {
-                erroEncontrado = erro;
-            }
-        }
-        return erroEncontrado;
+    for (let erro in campo.validity) {
+      if (erro != 'customError' && campo.validity[erro] && erro != 'valid') {
+        erroEncontrado = erro
+      }
     }
+    return erroEncontrado
+  }
 
-    const erro = verificandoErro()
+  const erro = verificandoErro()
 
-    if (erro) {
-        campo.setCustomValidity("Hey, não esquece de mim :(")
-        campo.style.border = "3px solid rgb(255, 56, 56)"
-        campo.focus()
-    } else {
-        campo.setCustomValidity("")
-        campo.style.border = "3px solid rgb(12, 165, 165)"
-    }
+  if (erro) {
+    campo.setCustomValidity('Hey, não esquece de mim :(')
+    campo.style.border = '3px solid rgb(255, 56, 56)'
+    campo.focus()
+  } else {
+    campo.setCustomValidity('')
+    campo.style.border = '3px solid rgb(12, 165, 165)'
+  }
 }
 
 for (let campo of campos) {
-    campo.addEventListener("invalid", event => {
-        event.preventDefault
-        validacao(event)
-    })
-    campo.addEventListener("blur", validacao)
+  campo.addEventListener('invalid', event => {
+    event.preventDefault
+    validacao(event)
+  })
+  campo.addEventListener('blur', validacao)
 }
 
-document.querySelector("form").addEventListener("submit", e => {
-    e.preventDefault()
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault()
 
-    async function lancaDados(email = "tailalima.ds@gmail.com") {
-        const valor = document.getElementById("valor").value
-        const valorPonto = valor.replace(',', '.')
-        const data = document.getElementById("data").value
-        const descricao = document.getElementById("desc").value 
-    
-        const rawResponse = await fetch(`http://localhost:3000/people/${email}/transaction`, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({email: `${email}`, valor: `${valorPonto}`, data: `${data}`, descricao: `${descricao}`})
-        });
-        const content = await rawResponse.json();
-    };
+  async function lancaDados(email = 'tailalima.ds@gmail.com') {
+    const valor = document.getElementById('valor').value
+    const valorPonto = valor.replace(',', '.')
+    const data = document.getElementById('data').value
+    const descricao = document.getElementById('desc').value
 
-    lancaDados()
+    const rawResponse = await fetch(`/people/${email}/transaction`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: `${email}`,
+        valor: `${valorPonto}`,
+        data: `${data}`,
+        descricao: `${descricao}`
+      })
+    })
+    const content = await rawResponse.json()
+  }
 
-    location.href = './tabela.html'
+  lancaDados()
+
+  location.href = './tabela.html'
 })
 
-const usuarioLogado = (id) => {
-    fetch(`http://localhost:3000/people/${id}`)
-        .then(x => x.text())
-        .then(JSON.parse)
-        .then(data => {
-            document.getElementById("usuario").innerText = data.pessoa.nome
-        })
+const usuarioLogado = id => {
+  fetch(`/people/${id}`)
+    .then(x => x.text())
+    .then(JSON.parse)
+    .then(data => {
+      document.getElementById('usuario').innerText = data.pessoa.nome
+    })
 }
 
-
-usuarioLogado("61b9fd250870d00cc83a7df8")
+usuarioLogado('61b9fd250870d00cc83a7df8')
